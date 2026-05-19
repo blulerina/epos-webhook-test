cea8b1e3195a7252ff80379a7855e328442c93f0
blulerina <e1157366@u.nus.edu>
2026-05-19 15:20:01 +0800

    change groq model

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.post('/', async (req, res) => {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          model: 'llama3-8b-8192',
+          model: 'llama-3.3-70b-versatile',
           messages: [
             {
               role: 'system',
